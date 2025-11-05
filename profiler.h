eaad9305b3b12ab80d7e4a1031be7f8a80242f25
sibeni-li <log@sibeni.li>
2025-11-05 15:43:45 +0100

    Error codes added for more flexible handling

--- profiler.h
+++ profiler.h
@@ -1,11 +1,20 @@
 #ifndef PROFILER_H
 #define PROFILER_H
 
+#define SUCCESS 0
+#define ERROR_MEMORY_ALLOCATION 1
+#define ERROR_NOT_INITIALIZED 2
+#define ERROR_NULL_VALUE 3
+#define ERROR_ALREADY_RUNNING 4
+#define ERROR_NOT_RUNNING 5
+#define ERROR_NOT_FOUND 6
+#define ERROR_COULD_NOT_OPEN 7
+
 // Public prototypes
-void profiler_init(void);
-void profiler_start(char *fct_name);
-void profiler_stop(char *fct_name);
-void profiler_save_data(void);
-void profiler_cleanup(void);
+int profiler_init(void);
+int profiler_start(char *fct_name);
+int profiler_stop(char *fct_name);
+int profiler_save_data(void);
+int profiler_cleanup(void);
 
 #endif
